1cafdcee9901408ca4e02a0643b7fee3d8099f3d
Javier <javier.br@yahoo.com>
2022-09-06 00:54:41 -0300

    First delivery from exercise 1 from 2nd chapter

---
+++ src/com/kean/main/Main.java
@@ -0,0 +1,17 @@
+package com.kean.main;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Javier Bravo";
+        int age = 26;
+        double weight = 66.4;
+        boolean male = true;
+        long birthdate = 19951006;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(male);
+        System.out.println(birthdate);
+    }
+}
